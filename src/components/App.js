d247adcf0c40d7597c6c6a89e26278f4538bb093
MJ Aguirre <mjaguirrexx@gmail.com>
2021-11-29 21:10:07 +0800

    Adds footer and other styling

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, NavLink } from "react-router-dom";
 import logo from "../assets/bnmlogo.svg";
 import cardblack from "../assets/Front - Black.png"
 import { Nav } from "react-bootstrap";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -34,18 +35,21 @@ function App() {
         <Route path="transactionhistory" element={<TransactionHistory />} />
         <Route path="signin" element={<SignIn />} />
       </Routes>
+
       
       
+      <Footer />
     </div>
   );
 }
 
+// DASHBOARD
 function Dashboard() {
   return (
     <>
       <main className="main-dash">
         <div className="dashboard">
-          <h2>Welcome to Bank of New Maharlika</h2>
+          <h2>Welcome to <span className="bankname-styling">BANK OF NEW MAHARLIKA</span></h2>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore esse quasi facere ratione impedit consequuntur iste tenetur officia cum ipsum. Accusamus, quibusdam provident voluptates natus velit fugit vero minus, praesentium eaque mollitia officiis delectus quia amet vel, obcaecati veritatis sunt laborum explicabo. Reiciendis recusandae, officia animi, natus accusantium et cumque quod perferendis veniam adipisci velit reprehenderit veritatis, ipsa id magni? Nulla id quis recusandae ea facere temporibus suscipit rerum quibusdam error, eveniet distinctio earum voluptatibus aut beatae! Fugit sed quam suscipit deserunt minus hic recusandae et tenetur sit libero corporis quisquam alias quaerat voluptatem nulla nostrum mollitia, cumque amet placeat?</p>
         </div>
         <div className="dashboard2">
@@ -56,53 +60,73 @@ function Dashboard() {
   );
 }
 
+// CLIENT
 function Client() {
   return (
-    <>
-      <main>
+    <div>
+      <div className="client">
         <h2>Client Database</h2>
-      </main>
-    </>
+      </div>
+      <div className="client">
+
+      </div>
+    </div>
   );
 }
 
+// NEW CLIENT
 function NewClient() {
   return (
-    <>
-      <main>
+    <div>
+      <div className="newclient">
         <h2>New Client</h2>
-      </main>
-    </>
+      </div>
+      <div>
+        
+      </div>
+    </div>
   );
 }
 
+// TRANSACT
 function Transact() {
   return (
-    <>
-      <main>
+    <div>
+      <div className="transact">
         <h2>Transact</h2>
-      </main>
-    </>
+      </div>
+      <div className="transact">
+        
+      </div>
+    </div>
   );
 }
 
+// TRANSACTION HISTORY
 function TransactionHistory() {
   return (
-    <>
-      <main>
+    <div>
+      <div className="transactionhistory">
         <h2>Transaction History</h2>
-      </main>
-    </>
+      </div>
+      <div className="transactionhistory">
+        
+      </div>
+    </div>
   );
 }
 
+// SIGN IN / LOG OUT
 function SignIn() {
   return (
-    <>
-      <main>
-        <h2>Sign In</h2>
-      </main>
-    </>
+    <div>
+      <div className="signin">
+        <h2>Have a great day!</h2>
+      </div>
+      <div>
+        
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Dashboard.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Dashboard = () => (
+    <div>
+        <h1>Dashboard</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore esse quasi facere ratione impedit consequuntur iste tenetur officia cum ipsum. Accusamus, quibusdam provident voluptates natus velit fugit vero minus, praesentium eaque mollitia officiis delectus quia amet vel, obcaecati veritatis sunt laborum explicabo. Reiciendis recusandae, officia animi, natus accusantium et cumque quod perferendis veniam adipisci velit reprehenderit veritatis, ipsa id magni? Nulla id quis recusandae ea facere temporibus suscipit rerum quibusdam error, eveniet distinctio earum voluptatibus aut beatae! Fugit sed quam suscipit deserunt minus hic recusandae et tenetur sit libero corporis quisquam alias quaerat voluptatem nulla nostrum mollitia, cumque amet placeat?</p>
+    </div> 
+)
