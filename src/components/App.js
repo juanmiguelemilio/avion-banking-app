c5777d035aab8b6ad75b0a637aa5fed2bef1446c
MJ Aguirre <mjaguirrexx@gmail.com>
2021-12-01 19:54:13 +0800

    Refactors App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, NavLink } from "react-router-dom";
-import logo from "../assets/bnmlogo.svg";
-import cardblack from "../assets/Front - Black.png"
 import { Nav } from "react-bootstrap";
+
+import logo from "../assets/bnmlogo.svg";
+
+import Dashboard from "./Dashboard";
+import Client from "./Client";
+import NewClient from "./NewClient";
+import Transact from "./Transact";
+import TransactionHistory from "./TransactionHistory";
 import Footer from "./Footer";
+
 
 function App() {
   return (
@@ -36,82 +43,12 @@ function App() {
         <Route path="signin" element={<SignIn />} />
       </Routes>
 
-      
-      
+      <Dashboard />
+      <Client />
+      <NewClient />
+      <Transact />
+      <TransactionHistory />
       <Footer />
-    </div>
-  );
-}
-
-// DASHBOARD
-function Dashboard() {
-  return (
-    <>
-      <main className="main-dash">
-        <div className="dashboard">
-          <h2>Welcome to <span className="bankname-styling">BANK OF NEW MAHARLIKA</span></h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore esse quasi facere ratione impedit consequuntur iste tenetur officia cum ipsum. Accusamus, quibusdam provident voluptates natus velit fugit vero minus, praesentium eaque mollitia officiis delectus quia amet vel, obcaecati veritatis sunt laborum explicabo. Reiciendis recusandae, officia animi, natus accusantium et cumque quod perferendis veniam adipisci velit reprehenderit veritatis, ipsa id magni? Nulla id quis recusandae ea facere temporibus suscipit rerum quibusdam error, eveniet distinctio earum voluptatibus aut beatae! Fugit sed quam suscipit deserunt minus hic recusandae et tenetur sit libero corporis quisquam alias quaerat voluptatem nulla nostrum mollitia, cumque amet placeat?</p>
-        </div>
-        <div className="dashboard2">
-          <img className="cardblack" src={cardblack} alt="bnm" />
-        </div>
-      </main>
-    </>
-  );
-}
-
-// CLIENT
-function Client() {
-  return (
-    <div>
-      <div className="client">
-        <h2>Client Database</h2>
-      </div>
-      <div className="client">
-
-      </div>
-    </div>
-  );
-}
-
-// NEW CLIENT
-function NewClient() {
-  return (
-    <div>
-      <div className="newclient">
-        <h2>New Client</h2>
-      </div>
-      <div>
-        
-      </div>
-    </div>
-  );
-}
-
-// TRANSACT
-function Transact() {
-  return (
-    <div>
-      <div className="transact">
-        <h2>Transact</h2>
-      </div>
-      <div className="transact">
-        
-      </div>
-    </div>
-  );
-}
-
-// TRANSACTION HISTORY
-function TransactionHistory() {
-  return (
-    <div>
-      <div className="transactionhistory">
-        <h2>Transaction History</h2>
-      </div>
-      <div className="transactionhistory">
-        
-      </div>
     </div>
   );
 }
